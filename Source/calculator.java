c7bb6764af06f8da1e638a130ebbc7b622559b86
Carlos Tuñón <camotubi@gmail.com>
2016-11-28 00:29:55 -0500

    agregar postfix function ( broken right now )

--- Source/calculator.java
+++ Source/calculator.java
@@ -1,15 +1,51 @@
+import java.util.ArrayList;
+import java.util.Stack;
 
 public class calculator {
+	private char numberChars[] = {'0','1','2','3','4','5','6','7','8','9'};
+	private char priorityOrder[] = {'^','*','/','-','+'};
 	
 	private String input;
 	
 	
-	public boolean parseInput()
+	public String postFix()
+	{
+		StringBuilder postfix = new StringBuilder(50);
+		Stack<Character> tempSt = new Stack<Character>();
+		char cChar;//Current char of the input
+		//reads each char of the input 
+		for(int i =0; i < this.input.length() ; i++)
+		{
+			cChar = this.input.charAt(i);
+			//Check if the cChar is a number
+			for(int x = 0 ; x < this.numberChars.length ; x++)
+			{
+				if(cChar == numberChars[x])
+				{
+					postfix.append(cChar);
+					break;
+				}
+				
+			}
+			
+			if(!tempSt.empty())
+			{
+				int x;
+				for(x =0; x < this.priorityOrder.length ; x++)
+				{
+					if(cChar == priorityOrder[x])
+						break;
+				}
+				if(tempSt.peek())
+			}
+			
+		}
+		return "";
+	}
+	public String parseInput()
 	{
 		//Remove all white space
-		this.input.replaceAll("\\s+","");
-		return false;
-		
+		return this.input.replaceAll("\\s+","");
 	}
 
 }
