4f4c53180fa2ccd24f803f0799e908a0c00898d2
Carlos Tuñón <camotubi@gmail.com>
2016-11-27 23:25:28 -0500

    initial commit

---
+++ Source/calculator.java
@@ -0,0 +1,15 @@
+
+public class calculator {
+	
+	private String input;
+	
+	
+	public boolean parseInput()
+	{
+		//Remove all white space
+		this.input.replaceAll("\\s+","");
+		return false;
+		
+	}
+
+}
