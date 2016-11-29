303f0fca204600f4d97c2d348163a5e5e65ac70a
Carlos Tuñón <camotubi@gmail.com>
2016-11-28 23:29:21 -0500

    add postfix conversion algorithm with parenthesis support and add gitignore

--- Source/calculator.java
+++ Source/calculator.java
@@ -3,19 +3,23 @@ import java.util.Stack;
 
 public class calculator {
 	private char numberChars[] = {'0','1','2','3','4','5','6','7','8','9'};
-	private char priorityOrder[] = {'^','*','/','-','+'};
+	private char priorityOrder[] = {'+','-','/','*','^'};
+	private  char parenthesis[] = {'(',')'};
 	
-	private String input;
 	
+	private String input= "0+5*(7-2*(6+9))";
 	
+
 	public String postFix()
 	{
 		StringBuilder postfix = new StringBuilder(50);
 		Stack<Character> tempSt = new Stack<Character>();
+		boolean isCCharNum = false;
 		char cChar;//Current char of the input
 		//reads each char of the input 
 		for(int i =0; i < this.input.length() ; i++)
 		{
+			isCCharNum = false;
 			cChar = this.input.charAt(i);
 			//Check if the cChar is a number
 			for(int x = 0 ; x < this.numberChars.length ; x++)
@@ -23,24 +27,70 @@ public class calculator {
 				if(cChar == numberChars[x])
 				{
 					postfix.append(cChar);
+					isCCharNum = true;
 					break;
 				}
 				
 			}
-			
-			if(!tempSt.empty())
+			if(cChar == parenthesis[0])
 			{
-				int x;
-				for(x =0; x < this.priorityOrder.length ; x++)
+				tempSt.push(cChar);
+			}
+			else
+			{
+				if(cChar != parenthesis[1])
 				{
-					if(cChar == priorityOrder[x])
-						break;
+					if(!isCCharNum)
+					{	
+						if(!tempSt.empty() && tempSt.peek() != parenthesis[0])
+						{
+							int a,b;
+							boolean poped;
+							do
+							{
+								poped = false;
+								a=-1;
+								b=-1;
+								for(int x =0; x < this.priorityOrder.length ; x++)
+								{
+									if(cChar == priorityOrder[x])
+										a=x;
+									if(tempSt.peek() == priorityOrder[x])
+										b=x;
+								}
+								if(a!=-1 && b!=-1 && a<=b)
+								{
+									poped = true;
+									postfix.append(tempSt.pop());
+									
+								}
+							}while(!tempSt.empty() && poped );
+							tempSt.push(cChar);
+						}
+						else
+						{
+							System.out.println(cChar);
+							tempSt.push(cChar);
+						}
+	
+					}
 				}
-				if(tempSt.peek())
+				else
+				{
+					while(tempSt.peek()!=parenthesis[0])
+						postfix.append(tempSt.pop());
+					tempSt.pop();
+				}
 			}
 			
+			
 		}
-		return "";
+		System.out.println(tempSt.size());
+		while(tempSt.size()>0)
+		{
+			postfix.append(tempSt.pop());
+		}
+		return postfix.toString();
 	}
 	public String parseInput()
 	{
