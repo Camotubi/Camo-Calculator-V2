e6dfc59b3338b8d4a2044e878849454878c388db
Camotubi <camotubi@gmail.com>
2017-07-01 22:10:45 -0500

    no se que cambio

--- Source/Main.java
+++ Source/Main.java
@@ -1,7 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 	public static void main(String args[])
 	{
+		if(args.length == 0)
+			cli("");
+		else
+			if(args[0]=="-g")
+				gui();
+			else
+				cli(args[0]);
+				
+		
+	}
+
+	public static void cli(String input)
+	{
+		BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
 		calculator calc = new calculator();
-		System.out.println("\n"+calc.solve("10+3*3^2"));
+		if(input == "")
+			try
+			{
+				input = rd.readLine();
+			}
+			catch(IOException e)
+			{
+				System.out.println("Read issues");
+			}
+
+		System.out.println("\n"+calc.solve(input));
+	}
+
+	public static void gui()
+	{
+
 	}
 }
